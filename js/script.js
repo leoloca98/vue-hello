f0247eb9afcc5cf51de671db214db562232d26d9
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-27 13:16:43 +0200

    Add Vue config

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/*
+
+|Descrizione:
+Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1,
+utilizzando le properietà di Vue presenti in data.
+*Bonus
+Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
+#POST BONUS:
+- Chi non lo ha già fatto, recuperi e completi gli esercizi precedenti
+- Chi invece è in regola con la consegna degli esercizi può sperimentare,
+  in file separato, tutto quello che abbiamo visto oggi!
+
+*/
+
 console.log("Vue is here", Vue);
 
-Vue.config.devtools = true;  //*FONDAMENTALE SE NO NON VA
+Vue.config.devtools = true;  //*FONDAMENTALE SE NO NON VA IL TOOL
